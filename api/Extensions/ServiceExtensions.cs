36ac1ba887b8a4273e7860aa40e8359934eafa41
potaksoft <107770088+potaksoft@users.noreply.github.com>
2024-08-23 17:14:37 +0300

    Finance_Project_Part_1

---
+++ api/Extensions/ServiceExtensions.cs
@@ -0,0 +1,43 @@
+﻿using AspNetCoreRateLimit;
+
+namespace api.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureRateLimitingOptions(this IServiceCollection services)
+        {
+            var rateLimitRules = new List<RateLimitRule>()
+            {
+                new RateLimitRule()
+                {
+                    Endpoint = "*",
+                    Limit = 10,                               //1 dakikda en fazla 10 istek atilabilir
+                    Period = "1m"
+                }
+            };
+
+            services.Configure<IpRateLimitOptions>(opt =>
+            {
+                opt.GeneralRules = rateLimitRules;
+            });
+
+            services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
+            services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+            services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+            services.AddSingleton<IProcessingStrategy, AsyncKeyLockProcessingStrategy>();
+        }
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                //.WithExposedHeaders("X-Pagination")
+                );
+            });
+        }
+    }
+
+}
